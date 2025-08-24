3a2bc41621dafdad7231121143cd806f6620a201
leierx <larssmitheier@protonmail.com>
2025-08-24 23:24:53 +0200

    I have to fucking git

--- config/automaticUpgrades.nix
+++ config/automaticUpgrades.nix
@@ -8,23 +8,13 @@
   };
 
   programs.ssh = {
-    enable = true;
-
-    # Good to pin GitHub's host key (optional but recommended)
-    # knownHosts.github = {
-    #   hostNames = [ "github.com" ];
-    #   publicKey = "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-    # };
-
-    # Ensure the key is used for GitHub
-    matchBlocks."github.com" = {
-      host = "github.com";
-      user = "git";
-      identityFile = "/root/.ssh/id_ed25519_homelab_nixos";
-      identitiesOnly = true;
-      extraOptions = {
-        StrictHostKeyChecking = "accept-new";
-      };
-    };
+    extraConfig = ''
+      Host github.com
+        HostName github.com
+        User git
+        IdentityFile /root/.ssh/id_ed25519_homelab_nixos
+        IdentitiesOnly yes
+        StrictHostKeyChecking accept-new
+    '';
   };
 }
