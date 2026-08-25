74e048c9085e82a8b96bc6b089f65a12e7aa22d4
Lars Smith Eier <larssmitheier@protonmail.com>
2026-08-26 00:53:17 +0200

    envoy fun v2

--- nix/modules/haproxy.nix
+++ nix/modules/haproxy.nix
@@ -32,17 +32,17 @@
           default_backend be_drop_http
 
         backend be_prod
-          # Pinned k8s NodePorts of the Envoy Gateway data plane (the gateway's
-          # TLS/HTTP listeners surface on NodePort 30443/30080 on the master).
-          server prod-master 192.168.100.10:30443
+          # TEMP: real Envoy Gateway NodePorts (http/https) get set here once the
+          # data-plane Service has programmed (see gitops envoy-gateway app).
+          server prod-master 192.168.100.10:443
         backend be_test
-          server test-master 192.168.101.10:30443
+          server test-master 192.168.101.10:443
         backend be_prod_http
           mode http
-          server prod-master 192.168.100.10:30080
+          server prod-master 192.168.100.10:80
         backend be_test_http
           mode http
-          server test-master 192.168.101.10:30080
+          server test-master 192.168.101.10:80
         backend be_drop
           tcp-request content silent-drop
         backend be_drop_http
